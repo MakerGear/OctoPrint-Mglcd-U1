d962916a1d306cadc62b1b469a9d92e601bcb994
joshwills <joshewills@gmail.com>
2019-02-17 21:27:13 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ plugin_author = "MG|Josh"
 plugin_author_email = "josh@makergear.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/MakerGear/OctoPrint-Mglcd"
+plugin_url = "https://github.com/MakerGear/OctoPrint-Mglcd-U1"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
